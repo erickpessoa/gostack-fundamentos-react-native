b20381fcc11a4cdcc7e734acd8aba0673d9183fb
Erick Pessoa <erickpessoa@websisti.com.br>
2020-05-20 14:01:30 -0300

    Implementação da solução para o desafio 8 - Fundamentos do React Native.

--- src/hooks/cart.tsx
+++ src/hooks/cart.tsx
@@ -26,27 +26,100 @@ interface CartContext {
 const CartContext = createContext<CartContext | null>(null);
 
 const CartProvider: React.FC = ({ children }) => {
-  const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([] as Product[]);
 
   useEffect(() => {
     async function loadProducts(): Promise<void> {
       // TODO LOAD ITEMS FROM ASYNC STORAGE
+      const itemsCart = await AsyncStorage.getItem('@GoMarketPlace:cart');
+      if (itemsCart) {
+        setProducts([...JSON.parse(itemsCart)]);
+      }
     }
 
     loadProducts();
   }, []);
 
-  const addToCart = useCallback(async product => {
-    // TODO ADD A NEW ITEM TO THE CART
-  }, []);
+  const addToCart = useCallback(
+    async (product: Product) => {
+      // TODO ADD A NEW ITEM TO THE CART
+      const productCart = products.find(item => item.id === product.id);
 
-  const increment = useCallback(async id => {
-    // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      if (!productCart) {
+        setProducts([{ ...product, quantity: 1 }, ...products]);
+      } else {
+        setProducts([
+          { ...product, quantity: productCart.quantity + 1 },
+          ...products.filter(p => p.id !== productCart.id),
+        ]);
+      }
 
-  const decrement = useCallback(async id => {
-    // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
-  }, []);
+      await AsyncStorage.setItem(
+        '@GoMarketPlace:cart',
+        JSON.stringify(products),
+      );
+    },
+    [products],
+  );
+
+  const increment = useCallback(
+    async id => {
+      // TODO INCREMENTS A PRODUCT QUANTITY IN THE CART
+      const product = products.find(p => p.id === id);
+
+      if (product) {
+        setProducts(
+          products.map(item => {
+            const { quantity }: Product = item;
+            if (item.id === id) {
+              return {
+                ...item,
+                quantity: quantity + 1,
+              };
+            }
+            return item;
+          }),
+        );
+
+        await AsyncStorage.setItem(
+          '@GoMarketPlace:cart',
+          JSON.stringify(products),
+        );
+      }
+    },
+    [products],
+  );
+
+  const decrement = useCallback(
+    async id => {
+      // TODO DECREMENTS A PRODUCT QUANTITY IN THE CART
+      const product = products.find(p => p.id === id);
+      if (product) {
+        if (product.quantity === 1) {
+          setProducts(products.filter(item => item.id !== id));
+        } else {
+          setProducts(
+            products.map(item => {
+              const { quantity }: Product = item;
+              if (item.id === id) {
+                return {
+                  ...item,
+                  quantity: quantity - 1,
+                };
+              }
+              return item;
+            }),
+          );
+        }
+
+        await AsyncStorage.setItem(
+          '@GoMarketPlace:cart',
+          JSON.stringify(products),
+        );
+      }
+    },
+    [products],
+  );
 
   const value = React.useMemo(
     () => ({ addToCart, increment, decrement, products }),
--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -40,22 +40,30 @@ const Cart: React.FC = () => {
 
   function handleIncrement(id: string): void {
     // TODO
+    increment(id);
   }
 
   function handleDecrement(id: string): void {
     // TODO
+    decrement(id);
   }
 
   const cartTotal = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((acc, cur) => {
+      return acc + cur.quantity * cur.price;
+    }, 0);
 
-    return formatValue(0);
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const totalItens = products.reduce((acc, cur) => {
+      return acc + cur.quantity;
+    }, 0);
 
-    return 0;
+    return totalItens;
   }, [products]);
 
   return (
